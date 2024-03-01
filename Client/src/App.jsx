85a7c5f88dbbd0dbd2fe8dfca3aa63677c0552b8
kunal <kunalcivil20@gmail.com>
2024-03-01 22:13:48 +0530

    Create footer

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -6,6 +6,8 @@ import Projects from './pages/Projects'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 
 
@@ -22,6 +24,7 @@ export default function App() {
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/projects' element={<Projects />} />
     </Routes>
+    <Footer />
     </BrowserRouter>
   )
 }
