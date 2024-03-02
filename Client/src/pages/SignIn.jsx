f8c1bfd5686e8b507a3c18368820c4d5dd1800ef
kunal <kunalcivil20@gmail.com>
2024-03-02 19:05:24 +0530

    add redux toolkit

--- Client/src/pages/SignIn.jsx
+++ Client/src/pages/SignIn.jsx
@@ -1,26 +1,27 @@
 import {Alert,Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { Link,useNavigate } from 'react-router-dom';
 import { useState} from 'react';
+import { signInStart,signInFailure,signInSuccess } from '../redux/user/userSlice';
+import { useDispatch,useSelector } from 'react-redux';
 
 export default function SignIn() {
   const [formData,setFormData]= useState({});
-  const [errormessage,setErrorMessage] =useState(null);
-  const [loading,setLoading] = useState(false);
+const {loading,error:errormessage} =useSelector((state)=>state.user);
+const dispatch=useDispatch();
   const navigate = useNavigate();
 
   const handleChange =(e)=>{
     setFormData({...formData,[e.target.id]:e.target.value.trim() });
   
-  }
+  };
 
   const handleSubmit = async (e)=>{
     e.preventDefault();
     if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill out all fields')
+      return dispatch(signInFailure('Please fill out all fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+     dispatch(signInStart());
       const res =await fetch('/api/auth/signin',{
         method:'POST',
         headers:{'Content-Type':'application/json'},
@@ -28,17 +29,17 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        setLoading(false);
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
+     
       if(res.ok) {
+        dispatch(signInSuccess(data));
+        // console.log(data);
         navigate('/');
       }
 
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+        dispatch(signInFailure(error.message));
     }
 
   }
